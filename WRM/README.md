01aa89f539dd21420bec623a086f29cce0cddbe8
ehdtjr82 <ehdtjr83@nate.com>
2024-08-02 00:08:08 +0900

    Delete WRM directory

--- WRM/README.md
+++
@@ -1,7 +0,0 @@
-# WRM for WebSquare5 SP5 
-
-본 프로젝트는 WebSquare5 SP5 엔진을 기반으로 개발된 WRM(WebSquare Reference Model) 프로젝트입니다.
-
-테스트로 깃허브에 올려와요
-
-ㅇㅇㅇㅇ
--- WRM/src/com/inswave/wrm/sample/service/SessionService.java
+++
@@ -1,9 +0,0 @@
-package com.inswave.wrm.sample.service;
-
-public interface SessionService {
-    void createSession(String key, Object value);
-    Object getSession(String key);
-    void invalidateSession();
-    void setSessionExpireTime(int interval); 
-    int getSessionExpireTime();
-}
